7c7b952ef753c6363e2f039d1b6b818b09e95773
arhchatbot <140819782+arhchatbot@users.noreply.github.com>
2023-11-29 14:40:37 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(4);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Я Фронтен'
+});
